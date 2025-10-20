f7e81b2366531e6760a1de3c86f58ac834967eb6
nmt2779 <nguyenminhthong2779@gmail.com>
2025-10-20 17:00:15 +0700

    Test deploy

--- components/Game.tsx
+++ components/Game.tsx
@@ -161,7 +161,7 @@ export default function Game() {
         >
           <motion.ol
             key={`history-list-${historyVersion}`}
-            className="grid grid-cols-2 gap-3"
+            className="grid grid-cols-2 gap-2"
             initial="hidden"
             animate={showHistory ? "visible" : "hidden"}
             variants={{
